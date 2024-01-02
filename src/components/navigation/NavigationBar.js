3fda0fcb202be4745cbc280f4bb967def35775eb
miguel <rrennie66@gmail.com>
2024-01-02 00:08:34 -0500

    nav update

--- src/components/navigation/NavigationBar.js
+++ src/components/navigation/NavigationBar.js
@@ -7,7 +7,7 @@ const NavigationBar = ()=> {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Employees Shift Planner</Navbar.Brand>
+        <Navbar.Brand href="/employee-shift-planner">Employees Shift Planner</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
