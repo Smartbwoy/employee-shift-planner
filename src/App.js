c59d8a564461a828a21e47575ae999850a4578e7
miguel <rrennie66@gmail.com>
2025-05-03 20:59:10 -0500

    refactor: update routing in App.js to use BrowserRouter for development and HashRouter for production

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import './App.css';
 import React from 'react';
 import NavigationBar from './components/navigation/NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, BrowserRouter, Route, Routes } from 'react-router-dom';
 import ShiftPlanner from './components/shift-planner/ShiftPlanner';
 import Employee from './components/employee/Employee'
 import HomePage from './components/HomePage';
 
-
-
 function App() {
-   
+    // Use BrowserRouter for local development, HashRouter for production
+    const RouterComponent = process.env.NODE_ENV === 'development' ? BrowserRouter : Router;
+
     return (
-        <>
-        <NavigationBar/>
-        <Routes>
-            <Route exact path="/" element={<HomePage/>}/>
-            <Route exact path="/employee-shift-planner" element={<HomePage/>}/>
-            <Route path='/shiftplanner' element={<ShiftPlanner/>}/>
-            <Route path='/employee' element={<Employee/>}/>
-        </Routes>
-        </>
-        
+        <RouterComponent>
+            <NavigationBar/>
+            <Routes>
+                <Route exact path="/" element={<HomePage/>}/>
+                <Route exact path="/employee-shift-planner" element={<HomePage/>}/>
+                <Route path='/shiftplanner' element={<ShiftPlanner/>}/>
+                <Route path='/employee' element={<Employee/>}/>
+            </Routes>
+        </RouterComponent>
     )
 }
 
