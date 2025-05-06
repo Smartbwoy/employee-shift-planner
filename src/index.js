4b4b72b749ae3c0e71506304b704ef06937f7a8d
miguel <rrennie66@gmail.com>
2025-05-05 20:03:50 -0500

    refactor: remove unused HashRouter import from index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter,HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Create a Query Client instance
