077677f15d16d885381ccc4beebe60a3615ea6af
miguel <rrennie66@gmail.com>
2025-05-11 19:59:03 -0500

    feat: update API base URL for local development environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   api: {
-    baseUrl: 'https://employeeschedulerapi.azurewebsites.net/api',
+    //baseUrl: 'https://employeeschedulerapi.azurewebsites.net/api',
+    baseUrl: 'http://localhost:5113/api',
     endpoints: {
       employees: '/Employee',
       positions: '/position',
